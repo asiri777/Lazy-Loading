8fa273a401c6755186e824af8a226b1117e773e1
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-05 20:01:50 +0530

    Editing nav component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
-
 
 
 @NgModule({
   declarations: [HomeComponent, NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [HomeComponent, NavComponent]
 })
